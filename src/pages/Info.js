3d4d02dd50f94b38c1105fba0c3851ad65d0d735
Nwthompson <123703228+Nwthompson@users.noreply.github.com>
2025-03-05 12:46:51 -0500

    Start of info page setup

---
+++ src/pages/Info.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+
+function info(){
+    return(
+        <div className="info">
+			<header className="info-header">info</header>
+		</div>
+    )
+}
+export default info;
