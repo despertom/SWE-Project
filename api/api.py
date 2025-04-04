9efc8e62d2979b0adb23eb6dfcf3f40775efb919
despertom <147658764+despertom@users.noreply.github.com>
2025-04-04 14:08:27 -0400

    fix on chrome, and popup prompt

--- api/api.py
+++ api/api.py
@@ -3,8 +3,10 @@ from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from user.models import User
 from database import get_db
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 db = get_db()
 
