c3ed744ae58d98b0e02e3858db2b25aa1dba7a22
GregOlszyk <127216566+GregOlszyk@users.noreply.github.com>
2025-04-16 15:33:13 -0400

    Result display message added

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -7,8 +7,16 @@ function Result() {
     return (
         <div className="result">
             <h1 className="result-header">Result: {state.net_yearly || "N/A"} CO2/year</h1>
+            <h1 className="result-prompt">Your carbon footprint is {resultOutcome(20)}</h1>
         </div>
     );
+}
+
+function resultOutcome(carbonFootprint) {
+    if (carbonFootprint < 16) {
+        return "good";
+    }
+    return "bad";
 }
 
 export default Result;
